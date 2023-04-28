a38df004c054495227c4aa869797430fe3767174
graziellyf <132098875+graziellyf@users.noreply.github.com>
2023-04-28 17:09:14 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -35,6 +35,7 @@ function App() {
         <h2> Viagens </h2>
         <p> California- R$ 1200 <button onClick= {()=>comprapassagem(1200)}> Comprar </button></p>
         <p> Londres - R$ 1420 <button onClick= {()=>comprapassagem(1420)}> Comprar </button></p>
+        <p> França - R$ 1700 <button onClick= {()=>comprapassagem(1420)}> Comprar </button></p>
 
     </div>
   );
